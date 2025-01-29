268e0ca4e8aa74477222fe772b61cd7e3e4f3568
Ayoub Laroussi <ayoublaroussipro@gmail.com>
2025-01-29 13:45:49 +0100

    feat(backend): Set up multi-module Maven project structure

---
+++ backend/main-folder/business/src/test/java/co/simplon/business/BusinessApplicationTests.java
@@ -0,0 +1,13 @@
+package co.simplon.business;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
